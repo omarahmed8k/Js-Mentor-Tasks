e911e7068f8a9d8467eccc567c3028de15a39c57
Omar Ahmed ElSayed <omarahmed8k@gmail.com>
2021-07-04 18:19:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,19 +7,21 @@ let year = date.getFullYear();
 function myFunction() {
     let userDate = document.getElementById('age').value;
     let date = new Date(userDate);
-    // let userDay = date.getDate();
-    // let userMonth = date.getMonth() + 1;
+    let userMonth = date.getMonth() + 1;
     let userYear = date.getFullYear();
-    let age = year - userYear;
-    let months = age * 12;
-    let days = months * 30;
+    if (month < userMonth) {
+        age = year - userYear - 1;
+        months = age * 12 + month;
+        days = age * 365 + month * 31;
+    }
     let error = document.getElementById("error");
 
     if (isNaN(age) == false) {
         document.getElementById('years').innerHTML = age;
         document.getElementById('months').innerHTML = months;
         document.getElementById('days').innerHTML = days;
-        error.innerHTML = "";
+        error.innerHTML = '';
+;
     } else {
         error.innerHTML = "Please Add Your Birthdate";
     }
